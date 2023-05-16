59ab1d8f74f0bb3681e7448e1f96f288f711d333
ALexus <sumskoj01@gmail.com>
2023-05-16 15:12:53 +0300

    Finding products by titleLike method

--- src/main/java/com/etech/controller/ProductController.java
+++ src/main/java/com/etech/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,7 +19,11 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @GetMapping("all")
-    public List<Product> getProductList() {
-        return productRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+    public List<Product> getProductList(@RequestParam(name = "title", required = false) String title) {
+        if (title.isBlank())
+            return productRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+        else {
+            return productRepository.findAllByTitleContainsIgnoreCase(title);
+        }
     }
 }
