917ac302c994aad1faed882f671d85f3bd75c679
ALexus <sumskoj01@gmail.com>
2023-05-18 20:44:38 +0300

    category methods

--- src/main/java/com/etech/repository/CategoryRepository.java
+++ src/main/java/com/etech/repository/CategoryRepository.java
@@ -1,12 +1,16 @@
 package com.etech.repository;
 
 import com.etech.model.Category;
+import com.etech.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Long>,
         CrudRepository<Category, Long>, JpaRepository<Category, Long> {
+    List<Category> findAllByTitleContainsIgnoreCase(String title);
 }
